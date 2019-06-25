231915561c5a2f01081507bef8acd4f150ebfc03
Daniel Sullivan <dansullivan19@gmail.com>
2019-06-25 18:03:29 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,20 +31,34 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
+<<<<<<< HEAD
   "SELECT series.title
+=======
+  "SELECT series.title, COUNT(characters.species)
+>>>>>>> d24883e31e36e2a948d4c4d1926de53ce48c1bef
   FROM series
   INNER JOIN characters
   ON series.id = characters.series_id
   WHERE characters.species = \"human\"
+<<<<<<< HEAD
   GROUP BY series.title
+=======
+  GROUP BY characters.species
+  ORDER BY COUNT(characters.species) DESC 
+>>>>>>> d24883e31e36e2a948d4c4d1926de53ce48c1bef
   LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
+<<<<<<< HEAD
   "SELECT characters.name, COUNT(character_books.book_id) 
   FROM characters
   INNER JOIN character_books 
   ON characters.id = character_books.character_id
   GROUP BY characters.name
   ORDER BY COUNT(character_books.book_id) DESC"
+=======
+  "SELECT 
+  FROM"
+>>>>>>> d24883e31e36e2a948d4c4d1926de53ce48c1bef
 end
